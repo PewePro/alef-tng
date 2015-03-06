851a2595ca100c490eb37abf43b264516af31c62
Martin Svrček <mato.svrcek@gmail.com>
2015-03-06 18:50:13 +0100

    MS_TNG Devise model pre pouzivatela

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+	       :registerable,
+         :recoverable, 
+	       :rememberable,
+	       :trackable,
+	       :validatable
+
   has_many :setups_users
   has_many :user_to_lo_relations
   has_and_belongs_to_many :setups
