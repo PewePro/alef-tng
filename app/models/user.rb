15a6adb4be0cf1c25cd4384403409e9f0bf86f98
Martin Svrček <mato.svrcek@gmail.com>
2015-03-09 23:33:03 +0100

    MS_TNG uprava db, reg_formulara

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
 	       :trackable,
 	       :validatable
 
+  #validates :login, format: { with: /\A[A-Za-z0-9_]+\z/ }, presence: true, uniqueness: { case_sensitive: false }
+
   has_many :setups_users
   has_many :user_to_lo_relations
   has_and_belongs_to_many :setups
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306144552) do
+ActiveRecord::Schema.define(version: 20150309201154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20150306144552) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "aislogin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
