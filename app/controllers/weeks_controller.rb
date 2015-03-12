615dbdb861344612ea9e355f77de00c09908665b
Matus Pikuliak <matus.pikuliak@gmail.com>
2015-03-13 00:01:40 +0100

    Pridany infobox o danom tyzdni. Rozhodol som sa zmenit farebnu zmenu oproti navrhu, aby tam zapasoval progressbar.

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,6 +1,6 @@
 class WeeksController < ApplicationController
   def show
     @week = Week.find_by_number(params[:number])
-    @learning_objects = @week.learning_objects.distinct
+    @learning_objects = LearningObject.joins(:user_to_lo_relations).joins(concepts: :weeks).where('weeks.id = 1 AND user_to_lo_relations.user_id = 1')
   end
 end
