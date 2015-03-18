38a94865165d7227c45e76ba498dde0ce0823f64
Matus Pikuliak <matus.pikuliak@gmail.com>
2015-03-18 01:49:17 +0100

    Dokoncena kompletna funkcionalita obrazovky tyzdna

--- app/assets/javascripts/progressbar.js
+++ app/assets/javascripts/progressbar.js
@@ -3,6 +3,9 @@ var Progressbar = {
       $.each( $('.week-progressbar'), function( _, progressbar ) {
         var numberAll = $(progressbar).data('number-all');
         var numberDone = $(progressbar).data('number-done');
+        var percents = Math.floor(numberDone/numberAll * 100)
+        var caption = $(progressbar).find('.progressbar-caption');
+        caption.html("Dokončené " + numberDone + "/" + numberAll + " (" + percents + "%)");
         $(progressbar).progressbar({
            value: numberDone,
            max: numberAll
--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,36 +1,23 @@
 class WeeksController < ApplicationController
   def show
-    @week = Week.find_by_number(params[:number])
-    @learning_objects = LearningObject.joins(concepts: :weeks).where('weeks.id = ?',params[:number]).distinct
+    @setup = Setup.take
+    @week = @setup.weeks.find_by_number(params[:number])
+    @next_week = @week.next
+    @previous_week = @week.previous
 
-    # tri atributy: id learning objectu, typ vztahu a pocet takychto vztahov medzi LO a prihlasenym pouzivatelom
-    @relations = UserToLoRelation.find_by_sql('SELECT COUNT(*) AS count, lo_final.id AS learning_object_id, utlr.type FROM
+    @learning_objects = @week.learning_objects.distinct
 
-      (SELECT lo.id FROM learning_objects AS lo
-      JOIN concepts_learning_objects AS loc ON loc.learning_object_id = lo.id
-      JOIN concepts AS c ON c.id = loc.concept_id
-      JOIN concepts_weeks AS cw ON cw.concept_id = c.id
-      WHERE cw.week_id = 1
-      GROUP BY lo.id) AS lo_final
+    @relations = UserToLoRelation.
+            where('learning_object_id IN (?)', @learning_objects.map(&:id)).
+            where('user_id = ?', 1).
+            group(:learning_object_id, :type).count
 
-    JOIN user_to_lo_relations AS utlr ON utlr.learning_object_id = lo_final.id
-    WHERE utlr.user_id = 1
-    GROUP BY utlr.type, lo_final.id')
-
+    @lo_number_all = @learning_objects.count
+    @lo_number_done = @relations.select {|k,v| k[1] == "UserCompletedLoRelation"}.size
   end
 
   def list
-
-  end
-
-  private
-  ## Strong Parameters
-  def user_to_lo_relation_params
-    params.require(:user_to_lo_relation).permit(:count)
-  end
-
-  def learning_object_params
-    params.require(:learning_object).permit(:seen, :done)
+    # zoznam tyzdnov
   end
 
 end
