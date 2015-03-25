1ea029c708b0ba13373ec43e96535b388c41f594
Matus Pikuliak <matus.pikuliak@gmail.com>
2015-03-26 00:56:53 +0100

    Urobeny zaklad funkcionality obrazovky celeho setupu. Vytvorene a naformatovane view, zaklady controllera

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -7,13 +7,29 @@ class WeeksController < ApplicationController
 
     @learning_objects = @week.learning_objects.distinct
 
-    @relations = UserToLoRelation.get_basic_relations(@learning_objects, 1)
+    @relations = UserToLoRelation.get_basic_relations(@learning_objects, 1) # 1 = user_id
 
     @lo_number_all = @learning_objects.count
     @lo_number_done = @relations.select {|k,_| k[1] == "UserSolvedLoRelation"}.size
   end
 
   def list
-    # zoznam tyzdnov
+    @setup = Setup.take
+    @weeks = @setup.weeks
+
+    week_lo_ids = Array.new
+    @weeks.each do |x|
+      ids = Rails.cache.fetch('setup_' + x.id.to_s, {expires_in: 1.day}) do
+        x.learning_objects.pluck(:id)
+      end
+      week_lo_ids.push(ids.uniq)
+    end
+
+    learning_objects = Setup.take.learning_objects.distinct
+    relations = UserToLoRelation.get_basic_relations(learning_objects, 1)
+
+    @weeks_info = Array.new
+    # najdi pocet learning objectov pre week
+    # najdi pocet hotovych
   end
 end
