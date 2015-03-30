e8648856d223f65ef424447295dbfbbbd8835c04
Martin Svrček <mato.svrcek@gmail.com>
2015-03-30 22:40:10 +0200

    delete index from schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -96,9 +96,7 @@ ActiveRecord::Schema.define(version: 20150328152458) do
     t.boolean  "show_solutions",      default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["login"], name: "index_users_on_login", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "weeks", force: :cascade do |t|
     t.integer "setup_id"
