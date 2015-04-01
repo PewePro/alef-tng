778247dc154080b2ebc932a2309011b2fc20f05c
Matus Pikuliak <matus.pikuliak@gmail.com>
2015-04-02 00:13:27 +0200

    Osetrenie vytvarania tabulky Courses pre uz existujuce setupy a koncepty

--- db/migrate/20150401132203_add_courses.rb
+++ db/migrate/20150401132203_add_courses.rb
@@ -5,10 +5,18 @@ class AddCourses < ActiveRecord::Migration
       t.timestamps
     end
     remove_column :concepts, :setup_id
-    add_column :setups, :course_id, :integer, null: false
+    add_column :setups, :course_id, :integer
     add_column :concepts, :pseudo, :boolean, default: true, null: false
-    add_column :concepts, :course_id, :integer, null: false
+    add_column :concepts, :course_id, :integer
+
     add_foreign_key :setups, :courses
     add_foreign_key :concepts, :courses
+
+    course = Course.create!(name: 'Course One')
+    Setup.update_all(course_id: course.id)
+    Concept.update_all(course_id: course.id)
+
+    change_column :setups, :course_id, :integer, null: false
+    change_column :concepts, :course_id, :integer, null: false
   end
 end
