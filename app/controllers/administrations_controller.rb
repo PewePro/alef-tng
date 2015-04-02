1e7fafa9335caae4be9724047491d3af180734aa
Matus Pikuliak <matus.pikuliak@gmail.com>
2015-04-02 14:58:58 +0200

    Vytvorenie zakladneho controlleru, routy

--- app/controllers/administrations_controller.rb
+++ app/controllers/administrations_controller.rb
@@ -2,4 +2,9 @@ class AdministrationsController < ApplicationController
   authorize_resource :class => false
   def index
   end
+
+  def setup_config
+    @setup = Setup.find(params[:setup_id])
+    @concepts = @setup.course.concepts.includes(:weeks)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
 
   # Administracia
   get 'admin' => 'administrations#index', as: 'administration'
+  get 'admin/setup_config/:setup_id' => 'administrations#setup_config', as: 'setup_config'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
