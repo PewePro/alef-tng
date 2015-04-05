b3c702e227278bbc863a5abe0b59e4b8713b0ea4
Martin Svrček <mato.svrcek@gmail.com>
2015-04-05 15:09:36 +0200

    MS_TNG add column reference_id to table learning_objects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401132203) do
+ActiveRecord::Schema.define(version: 20150405130311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150401132203) do
     t.text     "question_text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "reference_id"
   end
 
   create_table "setups", force: :cascade do |t|
