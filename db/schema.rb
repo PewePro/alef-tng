dbae7dd054e9ff5479bdf367dbc3f13b93f6b7e7
Martin Svrček <mato.svrcek@gmail.com>
2015-04-05 13:57:30 +0200

    MS_TNG update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -91,33 +91,20 @@ ActiveRecord::Schema.define(version: 20150401132203) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "",        null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,         null: false
+    t.integer  "sign_in_count",       default: 0,         null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "aislogin"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,         null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
     t.string   "remember_token"
-    t.string   "role",                   default: "student", null: false
-    t.string   "encrypted_password",     default: "",        null: false
-    t.string   "type",                   default: "",        null: false
-    t.boolean  "show_solutions",         default: false
+    t.string   "role",                default: "student", null: false
+    t.string   "encrypted_password",  default: "",        null: false
+    t.string   "type",                default: "",        null: false
+    t.boolean  "show_solutions",      default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["login"], name: "index_users_on_login", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "weeks", force: :cascade do |t|
     t.integer "setup_id"
