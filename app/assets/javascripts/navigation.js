1f8fa2ff6a14ceb125b96590d95454007d205b7c
Matus Pikuliak <matus.pikuliak@gmail.com>
2015-04-10 00:46:10 +0200

    Schovavanie navigacie pri zoomovani

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,23 +1,58 @@
 var Nav = {
 
     ueHeight : null, //user element height
+    ue : null,
     nav : null,
 
-    initUserElement : function() {
+    startingWidth : null, // pouziva sa pri kontrole zoomu
+    lastWidth : null,
+
+    init : function() {
+
+        // nastavovanie vysok a umiestneni rozlicnych elementov
         this.nav = $('nav');
-        this.ueHeight = $('#user-element').outerHeight();
+        this.ue = $('#user-element');
+        this.ueHeight = this.ue.outerHeight();
         navHeight = this.nav.outerHeight();
         $('.nav-offset').css( "height", navHeight + this.ueHeight );
+        $('#faux-background').css( "height", '+='+this.ueHeight  );
 
-        $('#faux-background').css( "top", this.ueHeight  );
-
-
+        // nastavovanie spravania pri scrollovani
         Nav.autoScrollNav();
         $(window).scroll(function() {
             Nav.autoScrollNav();
         });
-
         $(document).scrollTop(this.ueHeight);
+
+        // nastavovanie spravania pri zoomovani
+        this.startingWidth = window.innerWidth;
+        this.lastWidth = window.innerWidth;
+        setInterval(this.checkZoom, 10);
+    },
+
+    checkZoom : function() {
+        var nowWidth = window.innerWidth
+
+        if (nowWidth == Nav.lastWidth) return;
+
+
+        if (nowWidth >= Nav.startingWidth && Nav.lastWidth < Nav.startingWidth) Nav.showNav();
+        if (nowWidth < Nav.startingWidth && Nav.lastWidth >= Nav.startingWidth) Nav.hideNav();
+
+        Nav.lastWidth = nowWidth;
+
+    },
+
+    showNav : function () {
+        this.nav.css('visibility','visible');
+        this.ue.css('visibility','visible');
+        $('.question-name, .nav-offset').css('visibility','visible');
+    },
+
+    hideNav : function() {
+        this.nav.css('visibility','hidden');
+        this.ue.css('visibility','hidden');
+        $('.question-name, .nav-offset').css('visibility','hidden');
     },
 
     autoScrollNav : function() {
