5da79462b449a09e52272aaadcdc8cd7f32f24e7
Matus Pikuliak <matus.pikuliak@gmail.com>
2015-04-17 01:07:45 +0200

    Popisok ku prepinacu

--- app/assets/javascripts/toggle.js
+++ app/assets/javascripts/toggle.js
@@ -1,16 +1,17 @@
 var Toggle = {
     initWeekToggle : function() {
-        $('#week-toggle .toggle-item').click(function() {
+        toggle = $('.toggle');
+        toggle.find('.toggle-item').click(function() {
             var id = $(this).attr('id');
 
             switch(id) {
                 case 'week-toggle-learning':
                     $.ajax({
-                    url: '/user/toggle-show-solutions',
-                    method: 'POST',
-                    data: {
-                        show_solutions: true
-                    }
+                        url: '/user/toggle-show-solutions',
+                        method: 'POST',
+                        data: {
+                            show_solutions: true
+                        }
                     });
                     break;
                 case 'week-toggle-testing':
@@ -23,16 +24,24 @@ var Toggle = {
                     });
                     break;
             }
-
+        });
+        toggle.find('.toggle-help-btn').click(function() {
+            Toggle.help(toggle);
         });
     },
 
-    reset : function(element) {
-        element.find('.toggle-item').removeClass('active');
+    reset : function(toggle) {
+        toggle.find('.toggle-item').removeClass('active');
     },
 
     select : function(toggle,itemName) {
         id = toggle.attr('id');
         $('#'+id+'-'+itemName).addClass('active');
+    },
+
+    help : function(toggle) {
+        id = toggle.attr('id');
+        $('#'+id+'-help').toggleClass('active');
     }
+
 }
