178d8a8cf3b8e76379fad7b876d9e21a95bcbb62
Martin Labaj <martin@labaj.sk>
2015-04-22 14:27:08 +0200

    add user email to feedback message

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -2,6 +2,6 @@ class FeedbackMailer < ApplicationMailer
   def new (feedback)
     @feedback = feedback
     @user = User.find(feedback.user_id)
-    mail(to: 'alef@fiit.stuba.sk', subject: 'Nový feedback od študenta')
+    mail(to: 'alef@fiit.stuba.sk', subject: '[ALEF:TNG] Nový feedback')
   end
 end
