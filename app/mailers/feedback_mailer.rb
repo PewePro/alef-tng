ae95ed5d91e0a1c76615b563588158b9ca6a8b5e
Martin Labaj <martin@labaj.sk>
2015-04-25 16:13:03 +0200

    add identifier to feedback mail subject, shorten URL

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,6 +1,6 @@
 class FeedbackMailer < ApplicationMailer
   def new (feedback)
     @feedback = feedback
-    mail(to: 'alef@fiit.stuba.sk', subject: "[ALEF:TNG] #{@feedback.url}")
+    mail(to: 'alef@fiit.stuba.sk', subject: "[ALEF:TNG] New msg: #{@feedback.url_path}")
   end
 end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,8 @@
 class Feedback < ActiveRecord::Base
   belongs_to :user
+
+  def url_path
+    URI.parse(self.url).path
+  end
+
 end
