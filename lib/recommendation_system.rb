630420837ee79d14ffe8c00daead393a2c6a23df
Matus Pikuliak <matus.pikuliak@gmail.com>
2015-04-27 00:55:44 +0200

    hybrid recommender hotovy

--- lib/recommendation_system.rb
+++ lib/recommendation_system.rb
@@ -1,16 +1,34 @@
 class RecommendationSystem
 
   def self.get_list (user_id, week_id)
-    id = RecommendationLinker.find_by(user_id: user_id, week_id: week_id).recommendation_configuration_id
-    config = RecommendationConfiguration.find(id)
 
-    config.recommenders_options.each do |r|
-      puts r.recommender.name
-      puts r.weight
+    # Najde prislusnu konfiguraciu odporucania
+    linker = RecommendationLinker.find_by(user_id: user_id, week_id: week_id)
+    if linker.nil?
+      config = RecommendationConfiguration.find_by_default(true)
+    else
+      config = RecommendationConfiguration.find(linker.recommendation_configuration_id)
     end
-    # get recommendation config
-    # compute all recs
-    # reduce and return
+
+    # Vytvori list, do ktoreho sa budu ukladat vysledky odporucani
+    list = Hash.new
+    Week.find(week_id).learning_objects.ids.uniq.each do |id|
+      list[id] = 0
+    end
+
+    # Necha prebehnut vsetky odporucace a ich vysledky zratava dokopy
+    unless config.nil?
+      config.recommenders_options.each do |r|
+        r_class = Object.const_get "#{r.recommender.name.capitalize}Recommender"
+        result = r_class.get_list(user_id,week_id)
+        result.each do |id, value|
+          list[id] += value * r.weight
+        end
+      end
+    end
+
+    # Vrati vysledny list
+    list.sort
   end
 
   def self.get_best (user_id, week_id)
