035a04f350e09cd31a2d9e7085a246212dfa58c2
Matus Pikuliak <matus.pikuliak@gmail.com>
2015-05-09 01:24:26 +0200

    Vytvaranie modelu pouzivatela a vypocty uspesnosti

--- db/migrate/20150508132143_create_activity_recommender_records.rb
+++ db/migrate/20150508132143_create_activity_recommender_records.rb
@@ -8,8 +8,11 @@ class CreateActivityRecommenderRecords < ActiveRecord::Migration
       t.integer :wrong_answers, null: false, default: 0
     end
 
+    add_index :activity_recommender_records, [:learning_object_id, :relation_learning_object_id, :relation_type]
+
     add_foreign_key :activity_recommender_records, :learning_objects
 
     add_column :user_to_lo_relations, :activity_recommender_check, :boolean, default: false
+
   end
 end
--- lib/recommender_system/activity_recommender.rb
+++ lib/recommender_system/activity_recommender.rb
@@ -21,26 +21,60 @@ module RecommenderSystem
 
 
     def get_model
-      relations = UserToLoRelation.where('user_id = (?) AND created_at > (?)', self.user_id, Date.today - 1.day)
+      recent_activity = UserToLoRelation.where('user_id = (?) AND created_at > (?)', self.user_id, Date.today - 1.day).to_a
 
-      # odpal ak je tam niekde hodinova medzera
+      return [] if recent_activity.last.created_at < Date.today - 1.hour
 
-      # odpal tie nadbytocne
+      # Zoberie poslednu aktivitu pouzivatela z dnesneho dna a odstrihne vsetky relacie, ktore od zvysku oddeluje aspon hodinova pauza
+      model = Array.new
+      begin
+        model << recent_activity.pop
+      end while model.last.created_at - 1.hour < recent_activity.last.created_at
 
-      relations
+      # Odstrani nadbytocne relacie Visited
+      (0..(model.count - 1)).reverse_each do |i|
+        if  model[i].type == 'UserVisitedLoRelation' and
+            (model[i+1].type == 'UserSolvedLoRelation' or model[i+1].type == 'UserFailedLoRelation' or model[i+1].type == 'UserDidntKnowLoRelation') and
+            model[i].learning_object_id == model[i+1].learning_object_id
+
+          model.delete_at i
+        end
+      end
+
+       # Odstrani nadbytocne relacie Solved
+      (1..(model.count)).reverse_each do |i|
+        if  model[i].type == 'UserSolvedLoRelation' and
+            (model[i-1].type == 'UserViewedSolutionLoRelation' or model[i-1].type == 'UserFailedLoRelation' or model[i-1].type == 'UserDidntKnowLoRelation') and
+            model[i].learning_object_id == model[i-1].learning_object_id
+
+          model.delete_at i
+        end
+      end
+
+      model
     end
 
 
     def get_probability (learning_object, relation)
-      activity_rate = ActivityRecommenderRecord.where(
+      activity = ActivityRecommenderRecord.where(
           learning_object_id: learning_object.id,
           relation_learning_object_id: relation.learning_object_id,
           relation_type: relation.type
-      ).select('right_answers / (right_answers + wrong_answers)')
+      )
 
-      success_rate = learning_object.right_answers.to_f / (learning_object.right_answers + learning_object.wrong_answers)
+      activity_rate = get_success_rate activity
+      success_rate = get_success_rate learning_object
 
       activity_rate - success_rate
+    end
+
+
+    def get_success_rate obj
+      if obj.right_answers == 0
+        0
+      else
+        obj.right_answers.to_f / (obj.right_answers + obj.wrong_answers)
+      end
     end
 
 
