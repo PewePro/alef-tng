737282d66f130ab588a2d1bc69c4ea14e55d58a0
petergaspar <skpepek92@gmail.com>
2016-02-12 19:46:32 +0100

    Added feedback count info

--- app/controllers/administrations_controller.rb
+++ app/controllers/administrations_controller.rb
@@ -44,10 +44,12 @@ class AdministrationsController < ApplicationController
     @questions = @course.learning_objects.eager_load(:answers)
 
     feedback_new_count = Feedback.where(accepted: nil).where.not(learning_object_id: nil).count
-    feedback_aggs = feedback_new_count > 0 ? Feedback.select("learning_object_id").where(accepted: nil).group(:learning_object_id).count : {}
+    feedback_all_aggs = Feedback.select("learning_object_id").group(:learning_object_id).count
+    feedback_new_aggs = feedback_new_count > 0 ? Feedback.select("learning_object_id").where(accepted: nil).group(:learning_object_id).count : {}
     @feedbacks = {
-        aggs: feedback_aggs,
-        count: feedback_new_count
+        all_aggs: feedback_all_aggs,
+        new_aggs: feedback_new_aggs,
+        new_count: feedback_new_count
     }
   end
 
