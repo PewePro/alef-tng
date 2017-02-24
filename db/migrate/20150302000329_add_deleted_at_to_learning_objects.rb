39d35ab7a3cc49557739fa9bdf33bee6756bae71
Matus Pikuliak <matus.pikuliak@gmail.com>
2017-02-24 13:37:42 +0100

    Check if column already exists

--- db/migrate/20150302000329_add_deleted_at_to_learning_objects.rb
+++ db/migrate/20150302000329_add_deleted_at_to_learning_objects.rb
@@ -1,8 +1,10 @@
 class AddDeletedAtToLearningObjects < ActiveRecord::Migration
   def change
 
-    add_column :learning_objects, :deleted_at, :datetime
-    add_index :learning_objects, :deleted_at
+    unless column_exists? :learning_objects, :deleted_at
+      add_column :learning_objects, :deleted_at, :datetime
+      add_index :learning_objects, :deleted_at
+    end
 
   end
 end
